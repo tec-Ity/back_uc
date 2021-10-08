9e2fea1be6b9ed500843091322f91a491cfa3376
tec-Ity <tecitys@gmail.com>
2021-10-08 16:29:55 +0200

    categs

--- src/views/categ/list/Categs.js
+++ src/views/categ/list/Categs.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import makeStyles from "@mui/styles/makeStyles";
 import Container from "@mui/material/Container";
 import { Breadcrumbs, Grid, Link, Typography } from "@mui/material";
 import SearchInput from "../../../components/universal/query/SearchInput";
 import { useDispatch, useSelector } from "react-redux";
 import { getObjects } from "../../../features/objectsSlice";
-
+import api_DNS from "../../../js/_dns";
 const useStyle = makeStyles({
   root: {},
   //header
@@ -25,37 +25,80 @@ const useStyle = makeStyles({
     padding: "2px",
     boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.1)",
     borderRadius: "5px",
-    "&::before": {
-      content: '""',
-      width: "110px",
-      height: "40px",
-      border: "1px solid",
-      position: "absolute",
-      top: 0,
-      right: 0,
-    },
-    // backgroundColor: "black",
-    // clipPath:
-    //   "polygon(0% 15%, 0 0, 15% 0%, 85% 0%, 85% 20%, 100% 20%, 100% 100%, 85% 100%, 15% 100%, 0 100%, 0% 85%)",
+    // "&::after": {
+    //   content: '""',
+    //   position: "absolute",
+    //   top: 0,
+    //   right: 0,
+    //   height: "25%",
+    //   width: "10%",
+    //   backgroundColor: "#fff"
+    // },
+    // "&:hover": {
+    //   boxShadow: "none",
+    //   borderLeft: "2px solid #0000004d",
+    //   borderBottom: "2px solid #0000004d",
+    //   //right comp
+    //   "&::before": {
+    //     content: '""',
+    //     width: "10%",
+    //     height: "75%",
+    //     position: "absolute",
+    //     borderRadius: "0 5px 5px 0 ",
+    //     bottom: 0,
+    //     right: 0,
+    //     borderRight: "2px solid #0000004d",
+    //     borderTop: "2px solid #0000004d",
+    //   },
+    //   //top comp
+    //   "&:after": {
+    //     content: '""',
+    //     position: "absolute",
+    //     top: 0,
+    //     left: 0,
+    //     borderRadius: "5px 5px 0 0 ",
+    //     borderTop: "2px solid #0000004d",
+    //     borderRight: "2px solid #0000004d",
+    //     height: "40px",
+    //     width: "90%",
+    //   },
+    // },
   },
-  // innerBox: {
-  //   boxSizing: "border-box",
-  //   height: "100%",
-  //   width: "100%",
-  //   backgroundColor: "#fff",
-  //   clipPath:
-  //     "polygon(0% 15%, 0 0, 15% 0%, 85% 0%, 85% 20%, 100% 20%, 100% 100%, 85% 100%, 15% 100%, 0 100%, 0% 85%)",
-  // },
   iconGroup: {
     display: "flex",
     position: "absolute",
     right: "0",
+    top: 0,
     "& > div": {
       height: "30px",
       width: "30px",
-      backgroundColor: "#1d1d3840",
+      // backgroundColor: "#1d1d3840",
+      backgroundColor: "#fff",
       margin: "5px",
+      border: "2px solid #000",
+      borderRadius: "5px",
     },
+  },
+  childGroup: {
+    padding: "0 10%",
+  },
+  categBg: {
+    height: "100%",
+    width: "100%",
+    objectFit: "cover",
+    position: "absolute",
+    zIndex: "-1",
+  },
+  categCode: {
+    width: "fit-content",
+    height: "fit-content",
+    backgroundColor: "#fff",
+    fontSize: "30px",
+    fontWeight: "700",
+    borderRadius: "10px",
+  },
+  categUpload: {
+    backgroundColor: "#fff",
   },
 });
 
@@ -96,28 +139,103 @@ function CategList({ flagSlice }) {
 
 function CategListItem({ flagSlice, categ, index }) {
   const classes = useStyle();
-  const [modifying, setModifying] = React.useState(false);
+  const [modifying, setModifying] = useState(false);
+  const [showChild, setShowChild] = useState(false);
+  const ref = React.useRef();
   return (
-    <Grid container item xs={12} className={classes.listGridItem}>
-      {/* <div className={classes.innerBox}> */}
-      {(index + 1).toFixed(1) + categ.code}
-      {/* icon groups */}
-      <div className={classes.iconGroup}>
-        {modifying === true ? (
+    <>
+      <Grid
+        container
+        item
+        xs={12}
+        className={classes.listGridItem}
+        justifyContent='center'
+        alignItems='center'
+        onClick={() => {
+          modifying === true
+            ? ref.current.click()
+            : showChild === true
+            ? setShowChild(false)
+            : setShowChild(true);
+        }}>
+        {modifying === false ? (
           <>
-            <div onClick={() => setModifying(false)}>Done</div>
-            <div onClick={() => setModifying(false)}>Cancle</div>
-            <div>Del</div>
+            {/* bg img */}
+            <img
+              src={api_DNS + categ.img_url}
+              alt={categ.code}
+              className={classes.categBg}
+            />
+            {/* gateg code */}
+            <div className={classes.categCode}>
+              {(index + 1).toFixed(1) + categ.code}
+            </div>
           </>
         ) : (
-          <>
-            <div onClick={() => setModifying(true)}>edit</div>
-            <div>del</div>
-          </>
+          <div className={classes.categUpload}>
+            <div>Upload image{" (16:9)"}</div>
+            <input ref={ref} style={{ display: "none" }} type='file' />
+          </div>
         )}
-      </div>
-      {/* </div> */}
-    </Grid>
+        {/* icon groups */}
+        <div className={classes.iconGroup}>
+          {modifying === true ? (
+            <>
+              <div onClick={() => setModifying(false)}>Done</div>
+              <div onClick={() => setModifying(false)}>Cancle</div>
+              <div>Del</div>
+            </>
+          ) : (
+            <>
+              <div
+                onClick={() => {
+                  setModifying(true);
+                  setShowChild(false);
+                }}>
+                edit
+              </div>
+              <div>del</div>
+            </>
+          )}
+        </div>
+      </Grid>
+      {showChild === true && (
+        <Grid container item xs={12} className={classes.childGroup}>
+          {categ?.Categ_sons?.map((son) => (
+            <Grid container item xs={12}>
+              <Grid item xs={1}>
+                up
+              </Grid>
+              <Grid item xs={2}>
+                {son.sort}
+              </Grid>
+              <Grid item xs={6}>
+                {son.code}
+              </Grid>
+              <Grid item xs={2}>
+                useable
+              </Grid>
+              <Grid item xs={1}>
+                del
+              </Grid>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+      {modifying === true && (
+        <Grid container item xs={12}>
+          <Grid item xs={2}>
+            {index + 1}
+          </Grid>
+          <Grid item xs={8}>
+            {categ.code}
+          </Grid>
+          <Grid item xs={2}>
+            useable
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 }
 
