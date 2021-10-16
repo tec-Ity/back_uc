4a5ebe5a3cd2d40d050b6a3b4c5143485bbbeeb9
kelin <ecom.feng@gmail.com>
2021-10-16 13:41:36 +0200

    page init

--- src/views/pd/list/Pds.js
+++ src/views/pd/list/Pds.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router";
 // import { FormattedMessage } from "react-intl";
 import { useSelector, useDispatch } from "react-redux";
 
-import { selectObjects, getObjects } from "../../../features/objectsSlice";
+import { selectObjects, getObjects, setQuery, selectQuery } from "../../../features/objectsSlice";
 
 import { getRolePath } from "../../../js/conf/confUser";
 import ListPageHeader from "../../../components/basic/ListPageHeader";
@@ -31,6 +31,16 @@ export default function Pds(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const page = useSelector(selectQuery(flagSlice))?.page || 1;
+const pageClick = (val) =>(event) => {
+  dispatch(
+    setQuery({ flagSlice, query: { key: "page", val }, isReload: false })
+  );
+}
+useEffect(() => {
+  dispatch(getObjects({ flagSlice, api, isReload: true }));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [page]);
   return (
     <>
       <ListPageHeader
@@ -47,6 +57,25 @@ export default function Pds(props) {
           clickEvent={clickEvent}
         />
       </div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination pagination-lg">
+          <li className="page-item">
+            <button className="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span className="sr-only">Previous</span>
+            </button>
+          </li>
+          <li className="page-item"><button onClick={pageClick(1)} className="page-link" href="#">1</button></li>
+          <li className="page-item"><button onClick={pageClick(2)} className="page-link" href="#">2</button></li>
+          <li className="page-item"><button onClick={pageClick(3)} className="page-link" href="#">3</button></li>
+          <li className="page-item">
+            <button className="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span className="sr-only">Next</span>
+            </button>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
