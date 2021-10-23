6ca4b8e99111168772e27e1747b2554e5ca9ddca
stonehopper <stonehopper@outook.it>
2021-10-23 16:56:55 +0200

    Login fix

--- src/views/_index/HomePage.js
+++ src/views/_index/HomePage.js
@@ -13,48 +13,49 @@ export default function HomePage() {
     setFormdata((pre) => ({ ...pre, [type]: e.target.value }));
 
   const login = () => {
-    dispatch(reducerLogin({ formdata }));
+    dispatch(reducerLogin({ formdata: { system: formdata } }));
     hist.replace("/");
   };
 
   return (
-    <div className='container'>
-      <div className='my-3 row'>
-        <label htmlFor='code-ipt' className='col-sm-2 col-form-label'>
+    <div className="container">
+      <div className="my-3 row">
+        <label htmlFor="code-ipt" className="col-sm-2 col-form-label">
           code
         </label>
-        <div className='col-sm-10'>
+        <div className="col-sm-10">
           <input
-            type='text'
-            className='form-control'
-            id='code-ipt'
+            type="text"
+            className="form-control"
+            id="code-ipt"
             onChange={chgFormdata("code")}
             value={formdata.code}
           />
         </div>
       </div>
-      <div className='mb-3 row'>
-        <label htmlFor='pwd-ipt' className='col-sm-2 col-form-label'>
+      <div className="mb-3 row">
+        <label htmlFor="pwd-ipt" className="col-sm-2 col-form-label">
           Password
         </label>
-        <div className='col-sm-10'>
+        <div className="col-sm-10">
           <input
-            type='password'
-            className='form-control'
-            id='pwd-ipt'
+            type="password"
+            className="form-control"
+            id="pwd-ipt"
             onChange={chgFormdata("pwd")}
             value={formdata.pwd}
           />
         </div>
       </div>
-      <div className='mb-3 row'>
-        <div className='col-sm-2'></div>
+      <div className="mb-3 row">
+        <div className="col-sm-2"></div>
         <>
-          <div className='col-sm-10 text-right'>
+          <div className="col-sm-10 text-right">
             <button
-              className='btn btn-primary btn-block'
+              className="btn btn-primary btn-block"
               onClick={login}
-              type='submit'>
+              type="submit"
+            >
               Login
             </button>
           </div>
